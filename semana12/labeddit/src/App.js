c5e43bc64febc3dc366da6f07f9276de3bef64c3
suphessel <suhessel28@gmail.com>
2021-07-19 16:42:29 -0300

    Pages e componentes criados

---
+++ semana12/labeddit/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components"
+import Routes from "./Routes/routes"
+
+const App = () => {
+  return (
+    <div>
+     <Routes/>
+    </div>
+  );
+}
+
+export default App;

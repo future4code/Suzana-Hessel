b301d2186f57d5f2efda315c8b76a30f1c1a4a1f
suphessel <suhessel28@gmail.com>
2021-08-09 16:00:44 -0300

    Exercícios Node.Js

---
+++ semana15/exercicios-node/template-node-npm/src/exercicios/ex2.js
@@ -0,0 +1,13 @@
+/*a) Os parâmetros são acessados por meio do process.argv, que é um array que contém 
+os argumentos da linha de comando. 
+O primeiro índice será 'node', é o segundo índice será o nome do arquivo JavaScript. 
+Os próximos elementos serão quaisquer argumentos adicionais da linha de comando.*/
+
+//b)
+
+
+const nome = process.argv[2]
+const idade = Number(process.argv[3])
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+

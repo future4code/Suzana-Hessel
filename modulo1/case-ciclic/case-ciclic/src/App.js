6b651b7d9b3a1691343a54b5653bde41eca5df4c
suphessel <suhessel28@gmail.com>
2022-02-21 10:46:42 -0300

    Pages e components criadas

--- modulo1/case-ciclic/case-ciclic/src/App.js
+++ modulo1/case-ciclic/case-ciclic/src/App.js
@@ -1,8 +1,13 @@
+import ResultPage from "./ResultPage/ResultPage";
+import SimulatorPage from "./SimulatorPage/SimulatorPage";
 
 
 function App() {
   return (
     <div>
+      
+      <SimulatorPage/>
+   
     </div>
   );
 }
---
+++ modulo1/case-ciclic/case-ciclic/src/constants/url.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://api.mathjs.org/v4/"
+export const headers = {
+    "Content-Type": "application/json"
+}
